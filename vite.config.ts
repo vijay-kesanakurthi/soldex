1038e19c639056f7102719d663f0505aa552038e
dontaskit28 <jagutatarao28@gmail.com>
2024-02-07 14:34:25 +0530

    check

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import inject from "@rollup/plugin-inject";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [
+        inject({
+          Buffer: ["buffer", "Buffer"],
+          global: "globalThis",
+        }),
+      ],
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
